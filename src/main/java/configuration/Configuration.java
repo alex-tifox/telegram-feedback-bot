1ed1d1903e4e83ee2464b499ffe087b56c149720
Alexander <tilnyi.olexander@gmail.com>
2018-09-01 09:49:23 +0000

    Update Configuration.java

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -1,6 +1,6 @@
 package configuration;
 
 public class Configuration {
-    public static String BOT_TOKEN = "614182466:AAG1jgDM6Ho4zDI_7UMz7hwg_VvHNrO_meU";
-    public static String BOT_NAME = "TypeSmartBot";
+    public static String BOT_TOKEN = "Your bot token";
+    public static String BOT_NAME = "Your bot username";
 }
